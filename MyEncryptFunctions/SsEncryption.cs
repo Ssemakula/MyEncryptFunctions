f1640e34e3d0c0e4c2734c1e88525511535fd3bf
Ssemakula Mukiibi <Ssemakula@users.noreply.github.com>
2023-11-03 23:09:44 +0300

    Add project files.

---
+++ MyEncryptFunctions/SsEncryption.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Windows;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace SsEncryption
+{
+
+    // Will always give you the same encryption for the same string
+
+    public class EncryptMe
+    {
+        private const string EncryptionKey = "ThisismySeedText"; // You should use a strong, secret key
+        private const string InitializationVector = "1234567890123456"; // 16 bytes IV
+
+        public static string EncryptSs(string plainText)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(EncryptionKey);
+                aesAlg.IV = Encoding.UTF8.GetBytes(InitializationVector);
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+
+        public static string DecryptSs(string cipherText)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(EncryptionKey);
+                aesAlg.IV = Encoding.UTF8.GetBytes(InitializationVector);
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+namespace SsSecureEncryption
+{
+    public class SecureEncryptMe
+    {
+        // The encrypt and decrypt have to have either 16 or 32 characters
+        // Because of the password/key combination you'll not necessarily get the same encryption string for the same password
+        // But you should get the same encryption string for the same password/key combination
+        public static string SecureEncrypt(string plainText, string encryptionKey)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(encryptionKey);
+                aesAlg.GenerateIV(); // Generate a random IV for each encryption
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor();
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+                    // Combine IV and ciphertext for storage
+                    byte[] iv = aesAlg.IV;
+                    byte[] encryptedBytes = msEncrypt.ToArray();
+                    byte[] result = new byte[iv.Length + encryptedBytes.Length];
+                    Buffer.BlockCopy(iv, 0, result, 0, iv.Length);
+                    Buffer.BlockCopy(encryptedBytes, 0, result, iv.Length, encryptedBytes.Length);
+
+                    return Convert.ToBase64String(result);
+                }
+            }
+        }
+
+        public static string SecureDecrypt(string cipherText, string encryptionKey)
+        {
+            byte[] combined = Convert.FromBase64String(cipherText);
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(encryptionKey);
+                byte[] iv = new byte[aesAlg.BlockSize / 8];
+                byte[] encryptedBytes = new byte[combined.Length - iv.Length];
+
+                Buffer.BlockCopy(combined, 0, iv, 0, iv.Length);
+                Buffer.BlockCopy(combined, iv.Length, encryptedBytes, 0, encryptedBytes.Length);
+
+                aesAlg.IV = iv;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor();
+
+                using (MemoryStream msDecrypt = new MemoryStream(encryptedBytes))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
